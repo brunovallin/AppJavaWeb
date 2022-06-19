c3a8f9f0e96d02116c86a9ed8333a387ba94732b
Thiago Araujo <thiago.araujo72@etec.sp.gov.br>
2022-06-19 05:25:41 -0300

    feat: create dao produto

--- src/main/java/DAO/ProdutoDAO.java
+++ src/main/java/DAO/ProdutoDAO.java
@@ -1,59 +1,184 @@
 package DAO;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 
+import VO.Cargo;
 import VO.Produto;
-public class ProdutoDAO {
-	public ArrayList<Produto> getProdutos(){
-			
-		ArrayList <Produto> lst = new ArrayList();
-		Produto p= new Produto();
-		p.setCodigo(0);
-		p.setNome("Teste 1");
-		
-		lst.add(p);
-		Produto p2= new Produto();
-		p2.setCodigo(1);
-		p2.setNome("Teste 2");
+public class ProdutoDAO extends BaseDAO implements IDao {
 
-		lst.add(p2);
-		Produto p3= new Produto();
-		p3.setCodigo(2);
-		p3.setNome("Teste 3");
+	private Produto vo;
 
-		lst.add(p3);
-		Produto p4= new Produto();
-		p4.setCodigo(3);
-		p4.setNome("Teste Prod 4");
-
-		lst.add(p4);
+	public Produto getVo() {
+		return vo;
+	}
+	public void setVo(Produto vo) {
+		this.vo = vo;
+	}
+	
+	public ProdutoDAO() {
+	}
+	
+	public ProdutoDAO(Produto produto) {
+		this.vo = produto;
+	}
+	
+	public ArrayList<Produto> getProdutos() throws Exception{
+		abrir();
+		ArrayList <Produto> lst = new ArrayList<Produto>();
+		Statement s = getConn().createStatement(); 
+		String sql = "select id, nome, descricao, ean, categoria, preco, ativo from Produto";
+		ResultSet rs = s.executeQuery(sql);
+		while (rs.next()) { 
+			Produto produto = new Produto();
+			produto.setCodigo(rs.getInt(1));
+			produto.setNome(rs.getString(2));
+			produto.setDescricao(rs.getString(3));
+			produto.setEan(Integer.parseInt(rs.getString(4)));
+			produto.setCategoria(rs.getString(5));
+			produto.setPreco(Double.parseDouble(rs.getString(6)));
+			produto.setAtivo(Boolean.parseBoolean(rs.getString(7)));
+			lst.add(produto);
+		}
+		s.close();
+		rs.close();		
+		fechar();
 		return lst;
 	}
-	public ArrayList<Produto> findProdutos(String nome){
+	
+	public ArrayList<Produto> findProdutos(String nome)  throws Exception{
+		super.abrir();
 		
-		ArrayList <Produto> lst = new ArrayList();
-		Produto p= new Produto();
-		p.setCodigo(0);
-		p.setNome("Teste 1");
-		
-		lst.add(p);		
+		ArrayList <Produto> lst = new ArrayList<Produto>();
+		String sql = "select id, nome, descricao, ean, categoria, preco, ativo from Produto where nome like ?";
+		PreparedStatement preparedStmt = getConn().prepareStatement(sql);
+		preparedStmt.setString (1, "%".concat(nome).concat("%"));  
+		ResultSet rs = preparedStmt.executeQuery();
+		while (rs.next()) { 
+			Produto produto = new Produto();
+			produto.setCodigo(rs.getInt(1));
+			produto.setNome(rs.getString(2));
+			produto.setDescricao(rs.getString(3));
+			produto.setEan(Integer.parseInt(rs.getString(4)));
+			produto.setCategoria(rs.getString(5));
+			produto.setPreco(Double.parseDouble(rs.getString(6)));
+			produto.setAtivo(Boolean.parseBoolean(rs.getString(7)));
+			lst.add(produto);
+		}
+		preparedStmt.close();
+		rs.close();
+		fechar();
 		return lst;
 	}
-	public void newProduto(Produto p){
-		
-		
+
+
+	@Override
+	public void save() {
+		try {
+			System.out.println("megnow started insert ");
+			abrir();
+			String query = "insert into Produto(nome, descricao, ean, categoria, preco)"
+					+ " values (?, ?, ?, ?, ?)";
+			PreparedStatement preparedStmt = getConn().prepareStatement(query);
+			preparedStmt.setString(1, this.vo.getNome());
+			preparedStmt.setString(2, this.vo.getDescricao());
+			preparedStmt.setLong(3, this.vo.getEan());
+			preparedStmt.setString(4, this.vo.getCategoria());
+			preparedStmt.setDouble(5, this.vo.getPreco());
+			preparedStmt.execute();
+			fechar();
+			System.out.println("megnow stoped insert ");
+		} catch (SQLException e) {
+			System.out.println("megnow" + e.getMessage());
+		} catch (Exception e) {
+		}
 	}
-	public void editProduto(Produto p){
-		
-		
+
+	@Override
+	public void load() {
+		try {
+			abrir();
+
+			String sql = "select id, nome, descricao, ean, categoria, preco, ativo from Produto where id=?";
+
+			PreparedStatement preparedStmt = getConn().prepareStatement(sql);
+			preparedStmt.setInt(1, this.vo.getCodigo());
+			System.out.println("[LOAD] get cargo with id " + this.vo.getCodigo());
+
+			ResultSet rs = preparedStmt.executeQuery();
+			this.vo = new Produto();
+			while (rs.next()) { 
+				this.vo.setCodigo(Integer.parseInt(rs.getString(1)) );
+				this.vo.setNome(rs.getString(2));		
+				this.vo.setDescricao(rs.getString(3));
+				this.vo.setEan(rs.getLong(4));		
+				this.vo.setCategoria(rs.getString(5));
+				this.vo.setPreco(rs.getDouble(6));		
+				this.vo.setAtivo(rs.getBoolean(7));
+			}
+			rs.close();
+			fechar();
+		} catch(Exception e) {
+			System.out.println("[LOAD] ERROR - get Produto" + e);
+		}
 	}
-	public void deleteProduto(Produto p){
-		
-		
+
+	public boolean load(int id) {
+		boolean retorno = false;
+		try {
+			abrir();
+
+			String sql = "select id, nome, descricao, ean, categoria, preco, ativo from Produto where id=?";
+
+			PreparedStatement preparedStmt = getConn().prepareStatement(sql);
+			preparedStmt.setInt(1, id);
+
+			ResultSet rs = preparedStmt.executeQuery();
+			rs.last();
+			if(rs.getInt(1) > 0)
+				retorno = true;
+			rs.close();
+			fechar();
+		} catch(Exception e) {
+		}
+		return retorno;	
 	}
-	public Produto findByIdProduto(int id){
-		Produto p = new Produto();
-		p.setCodigo(2);
-		p.setNome("Teste 3");
-		return p;
+
+	@Override
+	public void edit(){
+		try {
+			abrir();
+
+			String query = " update Produto nome=?, descricao=?, ean=?, categoria=?, preco=?, ativo=? where id=?";
+			PreparedStatement preparedStmt = getConn().prepareStatement(query);
+			preparedStmt.setString(1, this.vo.getNome());
+			preparedStmt.setString(2, this.vo.getDescricao());
+			preparedStmt.setLong (3, this.vo.getEan());
+			preparedStmt.setString(4, this.vo.getCategoria());
+			preparedStmt.setDouble(5, this.vo.getPreco());
+			preparedStmt.setBoolean(6, this.vo.isAtivo());
+			preparedStmt.setInt(7, this.vo.getCodigo()); 
+			preparedStmt.execute();
+			fechar();
+		} catch (Exception e) {
+		}
 	}
+	@Override
+	public void delete() {
+
+		try {
+			abrir();
+			String query = " delete from Produto where id=?";
+			PreparedStatement preparedStmt = getConn().prepareStatement(query);
+			preparedStmt.setInt(1, this.vo.getCodigo());    
+			preparedStmt.execute();
+			preparedStmt.close();
+			fechar();
+		} catch (Exception e) {
+		}		
+
+	}
+
 }
